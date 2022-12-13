a8883424a2bb54c696a8119865b7d6cb21a4a304
ianrich69420 <ian.orieko@student.moringaschool.com>
2022-12-13 12:45:48 +0300

    Completed assignment

---
+++ README.md
@@ -0,0 +1,63 @@
+# Movie Theater!
+
+# Description
+
+This is the project made for the third code challenge for Moringa School.
+
+In this project is a website in which a user can:
+
+- See the first movie's details, including its **poster, title, runtime,
+   showtime, and available tickets** when the page loads.
+
+- See a menu of all movies on the left side of the page element when the page loads.
+
+-  Buy a ticket for a movie. After clicking the "Buy Ticket" button, I should
+   see the number of available tickets decreasing on the frontend. I should not
+   be able to buy a ticket if the showing is sold out (if there are 0 tickets
+   available). 
+
+## Extra Features
+
+## Project Setup
+
+In order for you to use the content on this repo ensure you have the following:
+
+- A computer that runs on Linux
+- nodejs installed
+- Any text editor such as VSCode installed
+- Any web browser such as Google Chrome installed
+
+## Instructions
+
+- Open a terminal on your computer
+- Clone the repo by using the following:
+
+        git clone https://github.com/ianrich69420/phase-1-wk-3-moringa-code-challenge.git
+
+- After the repository has been cloned succesfully change directory to the repository folder:
+
+        cd phase-1-wk-3-moringa-code-challenge
+
+- Open it in a text editor of your choice, however I personally prefer using VSCode so in order to open the folder in VSCode do the following:
+
+        code .
+
+You can use the following steps to run the app:
+
+- Install the required dependencies:
+
+        npm install --save json-server
+
+- Open the project in a browser of your choice, however I personally prefer using Google Chrome so in order to open the folder in Google Chrome do the following:
+
+        google-chrome index.html
+
+- Run the application
+
+        npm start
+
+- Go to the back to the page on your browser, refresh and vote for your favorite animal!
+
+## Author
+This project was contributed to by:
+- [Ian Richard Orieko](https://github.com/ianrich69420/)
---
+++ index.js
@@ -0,0 +1,90 @@
+function fetchMovies(){
+    return fetch("http://localhost:3000/films")
+    .then(res => res.json())
+    .then(json => getMovie(json))
+}
+
+function getMovie(movie){
+    let body = document.querySelector('body')
+    let firstMoviePoster = document.createElement('img')
+    firstMoviePoster.src = `${movie[0].poster}`
+    let firstMovieTitle = document.createElement('h2')
+    firstMovieTitle.textContent = `${movie[0].title}`
+    let firstMovieRuntime = document.createElement('p')
+    firstMovieRuntime.textContent = `Runtime: ${movie[0].runtime}`
+    let firstMovieShowtime = document.createElement('p')
+    firstMovieShowtime.textContent = `Showtime: ${movie[0].showtime}`
+    let availableTicketsForFirstMovieCount = movie[0].capacity - movie[0].tickets_sold
+    let availableTicketsForFirstMovie = document.createElement('p')
+    availableTicketsForFirstMovie.innerHTML = `Available Tickets: <span>${availableTicketsForFirstMovieCount}</span>`
+    let buyTicketButtonForFirstMovie = document.createElement('button')
+    buyTicketButtonForFirstMovie.type = `button`
+    buyTicketButtonForFirstMovie.id = `buy-ticket-button-for-first-movie`
+    buyTicketButtonForFirstMovie.textContent = `Buy Ticket`
+    buyTicketButtonForFirstMovie.addEventListener('click', () => {
+        if (availableTicketsForFirstMovieCount > 1) {
+            availableTicketsForFirstMovie.querySelector('span').textContent = --availableTicketsForFirstMovieCount
+        }
+        else {
+            availableTicketsForFirstMovie.querySelector('span').textContent = --availableTicketsForFirstMovieCount
+            buyTicketButtonForFirstMovie.textContent = `Sold Out`
+            document.querySelector('#buy-ticket-button-for-first-movie').disabled = true
+        }
+    })
+    let movieDetail = document.createElement('div')
+    movieDetail.id = "movie-detail"
+    body.appendChild(movieDetail)
+    movieDetail.appendChild(firstMoviePoster)
+    movieDetail.appendChild(firstMovieTitle)
+    movieDetail.appendChild(firstMovieRuntime)
+    movieDetail.appendChild(firstMovieShowtime)
+    movieDetail.appendChild(availableTicketsForFirstMovie)
+    movieDetail.appendChild(buyTicketButtonForFirstMovie)
+
+    movie.forEach(movie => {
+        let body = document.querySelector('body')
+        let moviePoster = document.createElement('img')
+        moviePoster.src = `${movie.poster}`
+        let movieTitle = document.createElement('li')
+        movieTitle.id = `title`
+        movieTitle.textContent = `${movie.title}`
+        let movieTitleDisplay = document.createElement('h2')
+        movieTitleDisplay.textContent = `${movie.title}`
+        let movieRuntime = document.createElement('p')
+        movieRuntime.textContent = `Runtime: ${movie.runtime}`
+        let movieShowtime = document.createElement('p')
+        movieShowtime.textContent = `Showtime: ${movie.showtime}`
+        let availableTicketCount = movie.capacity - movie.tickets_sold
+        let availableTickets = document.createElement('p')
+        availableTickets.innerHTML = `Available Tickets: <span>${availableTicketCount}</span>`
+        let buyTicketButton = document.createElement('button')
+        buyTicketButton.type = `button`
+        buyTicketButton.id = `buy-ticket-button`
+        buyTicketButton.textContent = `Buy Ticket`
+        buyTicketButton.addEventListener('click', () => {
+            if (availableTicketCount > 1) {
+                availableTickets.querySelector('span').textContent = --availableTicketCount
+            }
+            else {
+                availableTickets.querySelector('span').textContent = --availableTicketCount
+                buyTicketButton.textContent = `Sold Out`
+                document.querySelector('#buy-ticket-button').disabled = true
+            }
+        })
+
+        body.appendChild(movieTitle)
+        movieTitle.addEventListener('click', () => {
+            movieDetail.innerHTML = ``
+            movieDetail.appendChild(moviePoster)
+            movieDetail.appendChild(movieTitleDisplay)
+            movieDetail.appendChild(movieRuntime)
+            movieDetail.appendChild(movieShowtime)
+            movieDetail.appendChild(availableTickets)
+            movieDetail.appendChild(buyTicketButton)
+        })
+    })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchMovies();
+})
